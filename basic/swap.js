6c7f718b154ce715783d843bc9ee1f3cb5a20420
Miu <ispan8937@gmail.com>
2022-05-15 11:26:49 +0800

    update

---
+++ basic/swap.js
@@ -0,0 +1,19 @@
+let a = 1;
+let b = 2;
+console.log('before', a ,b); // 1,2
+
+let temp= a;
+a= b;
+b=temp;
+
+console.log('after', a, b); // 2, 1
+
+
+
+//---
+
+let person = {
+    name: 'Aming',
+    age: 25,
+    city: 'Taichung',
+  };
