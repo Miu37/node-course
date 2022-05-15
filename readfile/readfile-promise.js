ff749efd4fe9a06ea80d20a91a6e7efbe0c615cf
Miu <ispan8937@gmail.com>
2022-05-15 11:21:34 +0800

    edit

--- readfile/readfile-promise.js
+++ readfile/readfile-promise.js
@@ -1,21 +1,18 @@
+const fs = require("fs");
 
-    const fs = require('fs');
-    
-    
-let p = new Promise ((resolve, reject)=>{
-    fs.readFile('test11111.txt', 'utf-8', (err, data) => {
-
-      if (err) {
-        // 錯誤了
-        reject(err);
-      } else {
-        // 因為沒有 err，所以是正確的
-        resolve(data);
-      }
-      p.then((result)=>{
-          console.log(result)
-      }).catch((error)=>{
-          console.error(err);
-      });
-    });
+let p = new Promise((resolve, reject) => {
+  fs.readFile("text.txt", "utf-8", (err, data) => {
+    if (err) {
+      // 錯誤了
+      reject(err);
+    } else {
+      // 因為沒有 err，所以是正確的
+      resolve(data);
+    }
+  });
+});
+p.then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.error(error);
 });
