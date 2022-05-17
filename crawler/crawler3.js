974297ff4c410931779bc79bd3d49be9481dbdd6
Miu <ispan8937@gmail.com>
2022-05-17 16:30:34 +0800

    crawler.await.js

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,3 +1,31 @@
 // await version
 // 1. read stock no from file (fs)
 // 2. axios.get to request data
+
+const axios = require('axios');
+const fs = require('fs').promises;
+
+
+(async()=>{
+    
+    try{ 
+        let stockNo = await fs.readFile('stock.txt', 'utf-8')
+        console.log(stockNo);
+        let result= await axios
+              .get('https://www.twse.com.tw/exchangeReport/STOCK_DAY', {
+                params: {
+                  // 設定 query string
+                  response: 'json',
+                  date: '20220301',
+                  stockNo: stockNo,
+                },
+            });
+                console.log(result.data);
+
+    } catch(e) {
+        console.error(e);
+    };
+    })();
+
+
+   
